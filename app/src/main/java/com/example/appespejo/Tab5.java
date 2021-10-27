308a9d6bc8806357b94c1e5693809300030f5251
Laura <laurapamiesvillagordo@gmail.com>
2021-10-27 16:24:27 +0200

    mierda

--- app/src/main/java/com/example/appespejo/Tab5.java
+++ app/src/main/java/com/example/appespejo/Tab5.java
@@ -12,47 +12,25 @@ import androidx.fragment.app.Fragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
 public class Tab5 extends Fragment {
-
-//    private TextView username;
-      FirebaseUser usuario;
-      Button logout;
-      FirebaseAuth mAuth;
-      View HomeActivity;
-//    String hello = "";
-
-
-   /* @Override
+    /*@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }*/
-   public Tab5(){
-
-       usuario = FirebaseAuth.getInstance().getCurrentUser();
-       // require a empty public constructor
-   }
+    public Tab5(){
+        // require a empty public constructor
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view=inflater.inflate(R.layout.tab1, container, false);
-
-        mAuth = FirebaseAuth.getInstance();
-//        logout = (Button) view.findViewById(R.id.logOut);
-
-//        logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                logOut();
-//            }
-//        });
-
         return inflater.inflate(R.layout.tab5, container, false);
     }
-
-    private void logOut(){
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getActivity().getApplicationContext(), LoginActivity.class));
-    }
-
-
 }
+
